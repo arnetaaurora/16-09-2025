66ec4a50c65ecae40cf357390bc7b03a481df4ae
Arneta Aurora <arneta_widianto_34rpl@student.smktelkom-mlg.sch.id>
2025-09-16 10:21:05 +0700

    Add files via upload

---
+++ PerulanganDoWhile.java
@@ -0,0 +1,13 @@
+package AccountTest;
+
+public class PerulanganDoWhile {
+    public static void main(String[] args) {
+        int i=0;
+
+        do{
+            System.out.println("Perulangan ke"+i);
+            i++;
+        }while(i<=10);    
+    }
+    
+}
